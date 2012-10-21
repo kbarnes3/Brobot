1942a95257cc37f6f2aed29b360a631315e6b122
Kevin Barnes <kbarnes3@gmail.com>
2012-10-20 20:53:38 -0700

    More verbose output

--- arduino/accel_test/accel_test.ino
+++ arduino/accel_test/accel_test.ino
@@ -3,6 +3,10 @@
 #include "Arduino.h"
 
 const int sensorPin = A0;
+int cycle = 0;
+unsigned long nextOutputTime = 0;
+int minRead = 1024;
+int maxRead = 0;
 
 void setup()
 {
@@ -12,10 +16,27 @@ void setup()
 void loop()
 {
     int analog = analogRead(sensorPin);
-    char szOutput[6] = {};
+    if (analog < minRead)
+    {
+        minRead = analog;
+    }
+    if (analog > maxRead)
+    {
+        maxRead = analog;
+    }
 
-    sprintf(szOutput, "%d", analog);
-    Serial.println(szOutput);
-    delay(10);
+    unsigned long currentTime = millis();
+
+    if (currentTime > nextOutputTime)
+    {
+        char szOutput[35] = {}; // "Cycle: 999 - Min: 1024 - Max: 1024" with a null terminator
+
+        sprintf(szOutput, "Cycle: %d - Min: %d - Max: %d", cycle, minRead, maxRead);
+        Serial.println(szOutput);
+        cycle++;
+        nextOutputTime += 1000;
+        minRead = 1024;
+        maxRead = 0;
+    }
 }
 
