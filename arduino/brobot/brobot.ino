aaefc3d006452db15bdefb19e80d11d24632d715
Kevin Barnes <kbarnes3@gmail.com>
2012-10-26 21:38:22 -0700

    First attempt at reading the accelerometer

--- arduino/brobot/brobot.ino
+++ arduino/brobot/brobot.ino
@@ -18,5 +18,62 @@ void setup()
 
 void loop()
 {
+    measureAccel();
 }
 
+int measureAccel()
+{
+    int minValue = 1023;
+    unsigned long currentTick = 0;
+    unsigned long endTick = 0;
+    const unsigned long c_readTime = 250; // ms
+
+    currentTick = millis();
+    minValue = analogRead(accel);
+
+    // If the minValue is > 400 (512 is roughly 0 g),
+    // return immediately as nothing interesting is happening
+    if (minValue > 400)
+    {
+        return 0;
+    }
+
+    // Otherwise sample the accelerometer as many times as possible in the next 0.250 seconds
+    // and find the minimum
+    endTick  = currentTick + c_readTime;
+    while (currentTick < endTick)
+    {
+        int value = 1023;
+
+        value = analogRead(accel);
+        if (value < minValue)
+        {
+            minValue = value;
+        }
+
+        currentTick = millis();
+    }
+
+    // Now map minValue to a strength value between 1 and 5 and return that
+    if (minValue <= 50)
+    {
+        return 5;
+    }
+    else if (minValue <= 100)
+    {
+        return 4;
+    }
+    else if (minValue <= 200)
+    {
+        return 3;
+    }
+    else if (minValue <= 300)
+    {
+        return 2;
+    }
+    else //if (minValue <= 400)
+    {
+        return 1;
+    }
+
+}
