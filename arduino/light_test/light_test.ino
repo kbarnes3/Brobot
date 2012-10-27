9b8dd5c573ddebbfb77b9853ad002581b7ac0efa
Kevin Barnes <kbarnes3@gmail.com>
2012-10-27 14:03:18 -0700

    Updating light test

--- arduino/light_test/light_test.ino
+++ arduino/light_test/light_test.ino
@@ -15,11 +15,38 @@ void setup()
 
 void loop()
 {
-    for (int i = 0; i < cLights; i++)
-    {
-        digitalWrite(rgLights[i], HIGH);
-    }
+    setLights(LOW, 0);
 
-    delay(10000);
+    delay(1000);
+
+    setLights(HIGH, 200);
+
+    delay(500);
+
+    blinkLights();
+    blinkLights();
+    blinkLights();
+
+    delay(2000);
 }
 
+void setLights(int value, int msDelay)
+{
+    for (int i = 0; i < cLights; i++)
+    {
+        digitalWrite(rgLights[i], value);
+        delay(msDelay);
+    }
+}
+
+void blinkLights()
+{
+    const int msDelay = 400;
+
+    setLights(LOW, 0);
+    delay(400);
+
+    setLights(HIGH, 0);
+    delay(400);
+
+}
